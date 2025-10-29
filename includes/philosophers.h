fd57b3cf3514ec4a3c343b63d08cb6abc24ae166
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-10-29 12:16:27 -0300

    feat: philosophers header

---
+++ includes/philosophers.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 12:12:56 by mhidani           #+#    #+#             */
+/*   Updated: 2025/10/29 12:15:51 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# define SUCCESS 0x00
+# define FAILURE 0x01
+
+# define TRUE 0x01
+# define FALSE 0x00
+
+typedef char	t_bool;
+
+#endif
