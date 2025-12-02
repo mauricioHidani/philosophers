cfb31da95f6f8935be776e13d68e42a383b0b957
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-12-02 20:24:41 -0300

    feat: structures and funcs

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 16:33:06 by mhidani           #+#    #+#             */
-/*   Updated: 2025/12/02 17:22:32 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/12/02 20:22:26 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,42 @@
 # define FALSE 0x00
 
 typedef char	t_bool;
+
+typedef struct s_dining_table
+{
+	int				number_of_philos;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	long			number_of_meals;
+	pthread_mutex_t	print_mtx;
+	pthread_mutex_t	**forks_mtx;
+	pthread_t		death_monitor_thread;
+	t_philo			**philos;
+}			t_dining_table;
+
+typedef struct s_philo
+{
+	int				id;
+	pthread_t		thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	t_dining_table	*table;
+}	t_philo;
+
+// Dining Table Utilities ------------------------------------------------------
+t_dining_table	*ft_new_dining_table(char **srcs, int size);
+t_bool			ft_destroy_dining_table(t_dining_table *table);
+t_bool			ft_isvalid_dining_table(t_dining_table *table);
+
+// Forks Utilities -------------------------------------------------------------
+pthread_mutex_t	**ft_new_forks(t_dining_table *table);
+t_bool			ft_destroy_forks(pthread_mutex_t **forks, size_t size);
+void			ft_locate_forks_to_philo(t_philo *philo);
+
+// Philosopher Utilities -------------------------------------------------------
+t_philo			**ft_new_philos(t_dining_table *table);
+t_bool			ft_destroy_philos(t_philo **philos, size_t size);
 
 // Utilities -------------------------------------------------------------------
 void	ft_print_help(void);
