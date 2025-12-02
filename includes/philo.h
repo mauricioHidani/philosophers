e8a1ef8dfd833b8f863ff6568af173f855071100
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-12-02 17:10:54 -0300

    feat: ft print helper

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 16:33:06 by mhidani           #+#    #+#             */
-/*   Updated: 2025/12/02 16:33:47 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/12/02 16:52:32 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,6 @@
 # include <stdlib.h>
 # include <sys/time.h>
 # include <time.h>
+
+// Utilities -------------------------------------------------------------------
+void	ft_print_help(void);
