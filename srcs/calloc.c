5f3e76e3e68e3892e724469572cb4ce5f0b4971e
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-12-02 17:11:18 -0300

    feat: calloc

--- srcs/calloc.c
+++ srcs/calloc.c
@@ -1,24 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   calloc.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/02 16:33:06 by mhidani           #+#    #+#             */
-/*   Updated: 2025/12/02 17:11:01 by mhidani          ###   ########.fr       */
+/*   Created: 2025/12/02 17:07:50 by mhidani           #+#    #+#             */
+/*   Updated: 2025/12/02 17:10:08 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#include "philo.h"
 
-# include <pthread.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/time.h>
-# include <time.h>
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*new;
+	size_t	i;
 
-// Utilities -------------------------------------------------------------------
-void	ft_print_help(void);
-void	*ft_calloc(size_t nmemb, size_t size);
+	if (nmemb == 0 || size == 0)
+		return (malloc(1));
+	if (nmemb != 0 && size > (size_t)-1 / nmemb)
+		return (NULL);
+	new = malloc(nmemb * size);
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		((unsigned char *)new)[i] = 0;
+		i++;
+	}
+	return (new);
+}
