f97d4a33d1ed20c30b5a85587698e2596d6fe9d1
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-12-02 17:13:19 -0300

    feat: boolean type

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 16:33:06 by mhidani           #+#    #+#             */
-/*   Updated: 2025/12/02 17:11:01 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/12/02 17:13:04 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@
 # include <stdlib.h>
 # include <sys/time.h>
 # include <time.h>
+
+# define TRUE 0x01
+# define FALSE 0x00
+
+typedef char	t_bool;
 
 // Utilities -------------------------------------------------------------------
 void	ft_print_help(void);
