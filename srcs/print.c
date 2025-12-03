59126696e1541c5096c9ae8552408472069e5ca0
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-12-03 00:00:22 -0300

    feat: print funcs

---
+++ srcs/print.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 22:28:36 by mhidani           #+#    #+#             */
+/*   Updated: 2025/12/02 23:45:41 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_print_status(t_philo *philo, char *status)
+{
+	long	timestamp;
+
+	pthread_mutex_lock(&philo->table->lifetime_mtx);
+	if (philo->table->someone_died)
+	{
+		pthread_mutex_unlock(&philo->table->lifetime_mtx);
+		return ;
+	}
+	pthread_mutex_unlock(&philo->table->lifetime_mtx);
+	pthread_mutex_lock(&philo->table->print_mtx);
+	timestamp = ft_get_time() - philo->table->lifetime;
+	printf("🧐 Philospher %d : %s [%ldms]\n", philo->id, status, timestamp);
+	pthread_mutex_unlock(&philo->table->print_mtx);
+}
+
+void	ft_print_unverified(t_philo *philo, char *status)
+{
+	long	timestamp;
+
+	pthread_mutex_lock(&philo->table->print_mtx);
+	timestamp = ft_get_time() - philo->table->lifetime;
+	printf("🧐 Philospher %d : %s [%ldms]\n", philo->id, status, timestamp);
+	pthread_mutex_unlock(&philo->table->print_mtx);
+}
