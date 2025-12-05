b22508c511df51d960d5415794aa3cc0e1334d00
Mauricio Mityo Hidani <mhidani@c2r12p1.42sp.org.br>
2025-12-05 16:33:18 -0300

    fix: invalid prototype

--- srcs/print.c
+++ srcs/print.c
@@ -6,13 +6,11 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 22:28:36 by mhidani           #+#    #+#             */
-/*   Updated: 2025/12/05 16:26:12 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/12/05 16:32:54 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-static void	ft_print_locked(t_philo *philo, char *status);
 
 void	ft_print_status(t_philo *philo, char *status)
 {
