56503005bc9649890be2ffd732e83ae5ddab7e0a
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-28 21:27:40 -0300

    feat: atol

---
+++ src/utils/ft_atol.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/26 11:23:13 by mhidani           #+#    #+#             */
+/*   Updated: 2026/01/28 20:34:00 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+long	ft_atol(char *nbr)
+{
+	size_t	i;
+	long	num;
+	int		sign;
+
+	if (!nbr)
+		return (0);
+	i = 0;
+	num = 0;
+	sign = 1;
+	while (nbr[i] && ft_isifs(nbr[i]))
+		i++;
+	if (nbr[i] == '+' || nbr[i] == '-')
+	{
+		if (nbr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nbr[i])
+	{
+		num = num * 10 + nbr[i] - 48;
+		i++;
+	}
+	return (num * sign);
+}
