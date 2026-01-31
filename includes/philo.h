a68ddea49e9f220f9fbbde3a7bb4d7b1822c0b67
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-30 23:03:20 -0300

    feat: add action state

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 23:23:10 by mhidani           #+#    #+#             */
-/*   Updated: 2026/01/28 21:33:02 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/01/29 17:50:47 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@
 # include <sys/time.h>
 # include "type.h"
 # include "utils.h"
+
+# define MSG_TAKE_FORK "has taken a fork"
+# define MSG_EATING "is eating"
+# define MSG_SLEEPING "is sleeping"
+# define MSG_THINKING "is thinking"
+# define MSG_DIED "died"
 
 typedef pthread_mutex_t	t_mutex;
 typedef struct s_table	t_table;
