66c7489fe1140ff48de709bda6c4e178ecc65d35
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-30 23:01:10 -0300

    feat: monitor runtine

---
+++ src/maintainer/monitor_runtine.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor_runtine.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/29 18:07:56 by mhidani           #+#    #+#             */
+/*   Updated: 2026/01/29 18:22:24 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*monitor_runtine(void *ptr)
+{
+	t_table	*table;
+
+	if (!ptr)
+		return (NULL);
+	table = (t_table *)ptr;
+	while (TRUE)
+	{
+		
+	}
+	return (NULL);
+}
