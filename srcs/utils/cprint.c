3adc3daf26494b05f73e1ae42625a255e8262663
Mauricio Mityo Hidani <mhidani@c3r2p5.42sp.org.br>
2026-01-31 15:23:31 -0300

    fix: reference to info

--- srcs/utils/cprint.c
+++ srcs/utils/cprint.c
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 23:32:30 by mhidani           #+#    #+#             */
-/*   Updated: 2026/01/28 15:56:17 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/01/31 15:02:21 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ size_t	cprint(int fd, char *src)
 		return (0);
 	i = 0;
 	while (src[i])
-		i += write(fd, src[i], 1);
+		i += write(fd, &src[i], 1);
 	return (i);
 }
