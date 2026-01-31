7424daa67613723f0fbd75fe085c37a0edcb40cd
Mauricio Mityo Hidani <mhidani@c3r2p5.42sp.org.br>
2026-01-31 14:57:29 -0300

    reafct: change dir

--- srcs/starter/serve_meal_at.c
+++ srcs/starter/serve_meal_at.c
@@ -6,13 +6,13 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 10:42:31 by mhidani           #+#    #+#             */
-/*   Updated: 2026/01/29 18:15:58 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/01/31 13:57:25 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	start_simulation(t_table *table);
+static int	start_simulation(t_table *table);
 static void	join_threads(t_table *table);
 
 int	serve_meal_at(t_table *table)
@@ -23,25 +23,26 @@ int	serve_meal_at(t_table *table)
 
 	i = 0;
 	tidy_up_utensils(table);
-	start_simulation(table);
+	if (start_simulation(table) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
 	join_threads(table);
 	return (EXIT_SUCCESS);
 }
 
-static void	start_simulation(t_table *table)
+static int	start_simulation(t_table *table)
 {
 	size_t	i;
 	t_philo	**philos;
 	int		sts;
 
 	if (!table || (table && !table->philomenas))
-		return ;
+		return (EXIT_FAILURE);
 	i = 0;
 	philos = table->philomenas;
 	table->start_time = get_timestamp_ms();
 	while (philos[i] && !table->is_finished)
 	{
-		sts = pthread_create(philos[i]->thread, NULL, philo_runtine, philos[i]);
+		sts = pthread_create(&philos[i]->thread, NULL, philo_runtine, philos[i]);
 		if (sts != 0)
 		{
 			cpinfo(ERROR, STDERR_FILENO, ERRMSG_THREAD);
@@ -50,6 +51,7 @@ static void	start_simulation(t_table *table)
 		i++;
 	}
 	pthread_create(&table->monitor, NULL, monitor_runtine, table);
+	return (EXIT_SUCCESS);
 }
 
 static void	join_threads(t_table *table)
