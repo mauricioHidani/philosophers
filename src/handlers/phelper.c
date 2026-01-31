5994264e0faae29a39c6151db740fc6a60ebaf5c
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-30 22:51:41 -0300

    refact: check is printed

--- src/handlers/phelper.c
+++ src/handlers/phelper.c
@@ -6,21 +6,21 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 10:29:15 by mhidani           #+#    #+#             */
-/*   Updated: 2026/01/28 21:19:14 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/01/29 17:03:25 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-size_t	phelper(void)
+int	phelper(void)
 {
 	size_t	printed;
 	int		fd;
 
 	fd = STDERR_FILENO;
 	printed = 0;
-	printed += pinfo(fd, WARNING, "The input is incorrect!");
-	printed += pinfo(fd, INFO, "./bin/philo <1> <2> <3> <4> [5]");
+	printed += cpinfo(fd, WARNING, "The input is incorrect!");
+	printed += cpinfo(fd, INFO, "./bin/philo <1> <2> <3> <4> [5]");
 	cprint(fd, ITALIC);
 	printed += cprint(fd, "\t1. Amount philosophers [mandatory]\n");
 	printed += cprint(fd, "\t2. Time to die [mandatory]\n");
@@ -29,5 +29,10 @@ size_t	phelper(void)
 	printed += cprint(
 		fd, "\t5. Number of times each philosopher must eat [optional]\n");
 	cprint(fd, DEFAULT_COLOR);
-	return (cprint);
+	if (printed == 0)
+	{
+		cpinfo(ERROR, STDERR_FILENO, ERRMSG_PRINT);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
