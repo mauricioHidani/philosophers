917c32374b8a750b05db872d5da661b7998c7c18
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-01-30 22:55:28 -0300

    feat: new philomenas

--- src/structures/new_philos.c
+++ src/structures/new_philos.c
@@ -1,36 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   new_philo.c                                        :+:      :+:    :+:   */
+/*   new_philos.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/28 20:43:09 by mhidani           #+#    #+#             */
-/*   Updated: 2026/01/29 17:36:38 by mhidani          ###   ########.fr       */
+/*   Created: 2026/01/28 20:48:30 by mhidani           #+#    #+#             */
+/*   Updated: 2026/01/29 17:38:51 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-t_philo	*new_philo(t_table *table, int idx)
+t_philo	**new_philos(t_table *table)
 {
-	t_philo	*philo;
+	t_philo	**philos;
+	size_t	i;
 
 	if (!table)
 		return (NULL);
-	philo = ft_calloc(1, sizeof(t_philo));
-	if (!philo)
+	philos = ft_calloc(table->members + 1, sizeof(t_philo *));
+	if (!philos)
 	{
 		cpinfo(ERROR, STDERR_FILENO, ERRMSG_ALLOC);
 		return (NULL);
 	}
-	philo->id = idx + 1;
-	philo->meals = 0;
-	philo->last_meal = 0;
-	philo->table = table;
-	philo->right_fork = table->forks[idx];
-	philo->left_fork = table->forks[0];
-	if (table->forks[idx + 1])
-		philo->left_fork = table->forks[idx + 1];
-	return (philo);
+	i = 0;
+	while (i < table->members)
+	{
+		philos[i] = new_philo(table, i);
+		if (!philos[i])
+		{
+			destroy_philos(philos);
+			return (NULL);
+		}
+		i++;
+	}
+	return (philos);
 }
