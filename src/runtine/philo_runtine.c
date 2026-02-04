94322c972133944475ea6741342c43033605c208
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-02-04 12:14:17 -0300

    fix: start last meal and check satisfied

--- src/runtine/philo_runtine.c
+++ src/runtine/philo_runtine.c
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 13:46:19 by mhidani           #+#    #+#             */
-/*   Updated: 2026/02/03 15:11:16 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/02/04 11:04:20 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,15 @@ void	*philo_runtine(void *arg)
 	table = philo->table;
 	if (philo->id % 2 == 0)
 		usleep(1e3);
-	pthread_mutex_lock(&philo->meal);
-	philo->last_meal = get_timestamp();
-	pthread_mutex_unlock(&philo->meal);
-	while (!should_stop(table) || !philo->satisfied)
+	while (!should_stop(table))
 	{
+		pthread_mutex_lock(&philo->meal);
+		if  (philo->satisfied)
+		{
+			pthread_mutex_unlock(&philo->meal);
+			break ;
+		}
+		pthread_mutex_unlock(&philo->meal);
 		if (!philo_eat(philo))
 			break ;
 		philo_sleep(philo);
