786000cbfd1423ecce5f3721b42d8d898250a61e
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-02-05 11:14:05 -0300

    feat: philo runtine

--- src/runtine/philo_runtine.c
+++ src/runtine/philo_runtine.c
@@ -5,37 +5,85 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/03 13:46:19 by mhidani           #+#    #+#             */
-/*   Updated: 2026/02/04 11:04:20 by mhidani          ###   ########.fr       */
+/*   Created: 2026/02/04 21:41:41 by mhidani           #+#    #+#             */
+/*   Updated: 2026/02/05 09:53:48 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static t_bool	peat(t_philo *philo);
+static t_bool	psleep(t_philo *philo);
+static t_bool	pthink(t_philo *philo);
 
 void	*philo_runtine(void *arg)
 {
 	t_philo	*philo;
 	t_table	*table;
 
-	if (!arg)
-		return (NULL);
 	philo = (t_philo *)arg;
 	table = philo->table;
+	if (table->members == 1)
+	{
+		psafe(philo, TAKE_FORK);
+		wait_time(table, table->time_to_die);
+		return (NULL);
+	}
 	if (philo->id % 2 == 0)
-		usleep(1e3);
+		usleep(500);
 	while (!should_stop(table))
 	{
-		pthread_mutex_lock(&philo->meal);
-		if  (philo->satisfied)
-		{
-			pthread_mutex_unlock(&philo->meal);
+		if (!peat(philo))
 			break ;
-		}
-		pthread_mutex_unlock(&philo->meal);
-		if (!philo_eat(philo))
+		if (!psleep(philo))
 			break ;
-		philo_sleep(philo);
-		philo_think(philo);
+		if (!pthink(philo))
+			break ;
 	}
 	return (NULL);
+}
+
+static t_bool	peat(t_philo *philo)
+{
+	int		first;
+	int		second;
+	long	now;
+	t_table	*table;
+
+	table = philo->table;
+	if (should_stop(table))
+		return (FALSE);
+	find_forks(philo, &first, &second);
+	if (!take_forks(philo, first, second))
+		return (FALSE);
+	now = timestamp();
+	pthread_mutex_lock(&philo->meal);
+	philo->meals++;
+	pthread_mutex_unlock(&philo->meal);
+	pthread_mutex_lock(&philo->serving);
+	philo->last_meal = now;
+	pthread_mutex_unlock(&philo->serving);
+	psafe(philo, EAT);
+	wait_time(table, table->time_to_eat);
+	if (!down_forks(philo, first, second))
+		return (FALSE);
+	return (TRUE);
+}
+
+static t_bool	psleep(t_philo *philo)
+{
+	if (should_stop(philo->table))
+		return (FALSE);
+	psafe(philo, SLEEP);
+	wait_time(philo->table, philo->table->time_to_sleep);
+	return (TRUE);
+}
+
+static t_bool	pthink(t_philo *philo)
+{
+	if (should_stop(philo->table))
+		return (FALSE);
+	psafe(philo, THINK);
+	wait_time(philo->table, 1);
+	return (TRUE);
 }
